f168194890d47add75bad83fa4724adb7a84b22d
VidhitSikri <sikrividhit@gmail.com>
2026-02-09 17:37:59 +0530

    authentication using clerk fully implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import SafeSceen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeSceen>
+        <Slot />
+      </SafeSceen>
+    </ClerkProvider>
+  );
 }
