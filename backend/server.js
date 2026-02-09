e3fa36a6eb793474fc28027e8bcdce8d806badd4
VidhitSikri <sikrividhit@gmail.com>
2026-02-09 12:40:56 +0530

    setup neon db , clerk etc

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
+});
