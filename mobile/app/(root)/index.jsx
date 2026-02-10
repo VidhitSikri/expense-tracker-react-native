5cb212b5ee7b37cd53f5a426e40b74463bdee802
VidhitSikri <sikrividhit@gmail.com>
2026-02-10 14:44:00 +0530

    header of home page made

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,13 +1,15 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { Alert, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
 import { useTransactions } from "../../hooks/useTransactions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PageLoader from "../../components/PageLoader";
-
+import { styles } from "../../assets/styles/home.styles";
+import { Ionicons } from "@expo/vector-icons";
 export default function Page() {
   const { user } = useUser();
+  const router = useRouter();
   const { transactions, loadData, isLoading, summary, deleteTransaction } = useTransactions(user.id);
 
   useEffect(() => {
@@ -16,26 +18,42 @@ export default function Page() {
 
   if(isLoading) return <PageLoader />;
 
-
-
+  
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>Balance: {summary.balance}</Text>
-        <Text>Income: {summary.income}</Text>
-        <Text>Expenses: {summary.expense}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {/* LEFT SIDE */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+
+            <View>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0].emailAddress.split("@")[0]}
+              </Text>
+
+            </View>
+
+
+          </View>
+
+          {/* RIGHT SIDE */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
+              <Ionicons name="add" size={20} color="#FFF"/>
+              <Text>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+
+        </View>
+      </View>
     </View>
   );
 }
