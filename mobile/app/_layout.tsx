19cf55e2f25ce356e7b024f8845e8da1d77cd4bb
VidhitSikri <sikrividhit@gmail.com>
2026-02-10 15:37:48 +0530

    completed project!!!!

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from "expo-router";
 import SafeSceen from "../components/SafeScreen";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
@@ -9,6 +10,7 @@ export default function RootLayout() {
       <SafeSceen>
         <Slot />
       </SafeSceen>
+      <StatusBar style="dark" />
     </ClerkProvider>
   );
 }
